bce43fa10c3b9e099ea6c2124279982ac2548c58
kaslmineer7999 <kaslmineer7999github@outlook.com>
2025-02-15 05:21:34 +0000

    fix <script> and <style> tags protection

--- cgi-bin/msgs.sh
+++ cgi-bin/msgs.sh
@@ -4,5 +4,5 @@ echo -en "Content-type: text/html\nrefresh: 1\n\n"
 echo "<!doctype html>"
 echo "<style>*{font-family:serif;background-color:#1a1a1a;color:#FFFFFF}</style>"
 echo "<pre>"
-cat ../msg.txt|sed -e 's/\<style\>//g' -e 's/\<\/style\>//g' -e 's/\<script\>//g' -e 's/\<\/script\>//g'
+cat ../msg.txt|sed -e 's/<style>//g' -e 's|</style>||g' -e 's/<script>//g' -e 's|</script>||g'
 echo "</pre>"
